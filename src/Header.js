ab9b7bfc736a09c2b6f85a803d6a1c787e531f34
joshcard <66044735+joshcard@users.noreply.github.com>
2022-04-07 21:25:02 -0600

    final styling

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div>
-      <center><h1>MTG Collector</h1></center>
+      <center><h1 className='white'>MTG Collector</h1></center>
     </div>
   )
 }
